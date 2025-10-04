b37cf303b89d02a56ea3e89d18c58eb17c75f5f8
Yrekcaz <yrekcaz9@gmail.com>
2025-10-04 15:42:54 -0700

    Update config.ts

--- server/config.ts
+++ server/config.ts
@@ -11,10 +11,15 @@ export const notFoundRedirect: string = '/404';
 /**
  * Should the server redirect to a 404 on an invalid domain name?
  * If false, the server will return a message error.
+ 
+ * http[s]://subdomain.domain.tld/404 - if you wanted it on the root domain, and domain waffles.com, it would be https://waffles.com/404
+ * This can be any valid connected domain
  */
 export const invalidDomainRedirect: false | string = 'http://localhost/404';
 
 /**
  * Base domain name for the API.
+ * subdomain.domain.tld - if you wanted it on subdomain img and domain waffles.com, it would be img.waffles.com
+ * This must reflect the folder name in client
  */
-export const apiHostDomainName: string = 'api.localhost';
+export const apiHostDomainName: string = 'api.localhost'; 
